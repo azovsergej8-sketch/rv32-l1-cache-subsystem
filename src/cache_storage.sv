e22063732b873fc79573b3f329b7dcc540ffb0d2
azovsergej8-sketch <azovsergej8@gmail.com>
2026-05-30 11:12:51 +0300

    Update cache_storage.sv

--- src/cache_storage.sv
+++ src/cache_storage.sv
@@ -4,22 +4,22 @@ module cache_storage(
 );
 
   //Два массива памяти
-  reg[24:0] tag[64]; // Теги
+  reg[23:0] tag[64]; // Теги
   reg[31:0] data[64]; //Инструкции
   
   //FSM
   always@(posedge clk or negedge rst) begin
     if(!rst) begin
-      inf.storage_rvalid <= 0
+      inf.storage_rtag[0] <= 0;
     end else begin
       if(!inf.storage_we) begin
-        inf.storage_rtag <= tag[inf.storage_rindex];
+        inf.storage_rtag[23:0] <= tag[inf.storage_rindex];
         inf.storage_rdata <= data[inf.storage_rindex];
         inf.storage_rvalid <= 1;
       end else begin
         tag[inf.storage_windex] <= inf.storage_wtag;
         data[inf.storage_windex] <= inf.storage_wdata;
-        inf.storage_rvalid <= 0;
+        inf.storage_rtag[0] <= 0;
       end
     end
   end
