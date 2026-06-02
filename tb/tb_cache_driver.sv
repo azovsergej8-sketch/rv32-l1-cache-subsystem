dc2356a2e55fcfaefb2c5cb70da1ac2c50ac152a
azovsergej8-sketch <azovsergej8@gmail.com>
2026-06-03 01:05:50 +0300

    Create tb_cache_driver.sv

---
+++ tb/tb_cache_driver.sv
@@ -0,0 +1,22 @@
+class cache_driver;
+  malibox #(logic[31:0]) tr_fifo;
+  
+  function new( malibox #(logic[31:0]) addr_fifo);
+    this.tr_fifo = addr_fifo;
+  endfunction
+  
+  task drive(virtual core_intf cr_intf, event clk_e);
+    always begin
+      cache_transaction tr = new();
+      if(!tr.randomize()) $error("Randomization failed!");
+      @(clk_e);
+      if(ready_core) begin
+        if(!core_valid) begin
+          cr_intf.core_addr = tr.addr;
+          core_valid <= 1;
+          addr_fifo.put(tr.addr);
+        end else core_valid <= 0;
+      end
+    end
+  endtask
+endclass
