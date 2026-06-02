a8f331c69223d30196850885784ebbc329ccf3a9
azovsergej8-sketch <azovsergej8@gmail.com>
2026-06-03 01:14:59 +0300

    Update tb_cache_mem_model.sv

--- tb/tb_cache_mem_model.sv
+++ tb/tb_cache_mem_model.sv
@@ -1,5 +1,6 @@
 class cache_mem_model;
   protected logic[7:0] ram_table[logic[31:0]];
+  
   function logic[31:0] get_data(logic[31:0] addr);
     logic[31:0] result;
     for(int i = 0; i < 4; i++) begin
@@ -8,4 +9,14 @@ class cache_mem_model;
     result = {ram_table.exists(addr), ram_table.exists(addr + 1), ram_table.exists(addr + 2), ram_table.exists(addr + 3)};
     return result;
   endfunction
+
+  task run_mem_responder(virtual memory_intf mem_if, event e_clk);
+    always begin
+      @(clk_e);
+      if(mem_if.mem_valid) begin
+        mem_if.mem_rdata <= this.get_data(mem_addr);
+        mem_if.mem_ready <= 1;
+      end
+      if(mem_if.mem_ready) mem_if.mem_ready <= 0;
+    end
 endclass
