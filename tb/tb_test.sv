50db380fa2d2551db16778debf8b470f59f8d0b4
azovsergej8-sketch <azovsergej8@gmail.com>
2026-06-03 01:30:43 +0300

    Create tb_test.sv

---
+++ tb/tb_test.sv
@@ -0,0 +1,26 @@
+class test;
+  virtual core_intf cr_if;
+  virtual memory_intf mem_if;
+  cache_driver    drv;
+  cache_checker   chk;
+  cache_mem_model mem;
+  event clk_ev;
+
+  function new(virtual core_intf c_if, virtual memory_intf m_if,  malibox #(logic[31:0]) addr_fifo, event clk_e);
+    this.cr_if = c_if;
+    this.mem_if = m_if;
+    this.drv = new(addr_fifo);
+    this.mem = new();
+    this.clk_ev = clk_e;
+    this.chk = new(addr_fifo, this.mem, this.clk_ev);
+  endfunction
+
+  task run();
+    fork
+      drv.drive(cr_if, clk_ev);
+      mem.run_mem_responder(mem_if, clk_ev);
+      chk.true_model(cr_if);
+    join_any
+    $display("Тест завершен!");
+  endtask
+endclass
