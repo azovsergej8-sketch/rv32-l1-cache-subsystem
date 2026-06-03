6d6802289313e40331881fa844b13de182c61a7a
azovsergej8-sketch <azovsergej8@gmail.com>
2026-06-03 19:21:15 +0300

    Update tb_cache_driver.sv

--- tb/tb_cache_driver.sv
+++ tb/tb_cache_driver.sv
@@ -6,17 +6,20 @@ class cache_driver;
   endfunction
   
   task drive(virtual core_intf cr_intf, event clk_e);
-    forever begin
-      cache_transaction tr = new();
-      if(!tr.randomize()) $error("Randomization failed!");
+    cache_transaction tr;
+    repeat(50) begin
+      if(tr == null) begin
+        tr = new();
+        if(!tr.randomize()) $error("Randomization failed!");
+      end
       @(clk_e);
       if(cr_intf.ready_core) begin
-        cr_intf.core_addr <= tr.addr;
-        cr_intf.core_valid <= 1;
+        cr_intf.core_addr <= #1 tr.addr;
+        cr_intf.core_valid <= #1 1;
         tr_fifo.put(tr);
         tr.take_addr(tr.addr);
-      end
-      if(cr_intf.core_valid) cr_intf.core_valid <= 0;
+        tr = null;
+      end else cr_intf.core_valid <= 0;
     end
   endtask
 endclass
