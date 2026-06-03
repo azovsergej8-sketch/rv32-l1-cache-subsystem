0cda0f3b0bf20f56864faa1286d4eef2f6b77814
azovsergej8-sketch <azovsergej8@gmail.com>
2026-06-03 17:18:49 +0300

    Update tb_cache_checker.sv

--- tb/tb_cache_checker.sv
+++ tb/tb_cache_checker.sv
@@ -1,10 +1,10 @@
 class cache_checker;
-  mailbox #(logic[31:0]) addr_fifo;
+  mailbox #(cache_transaction) addr_fifo;
   cache_mem_model mem;
   event clk_ev;
   
   //Конструктор
-  function new(mailbox #(logic[31:0]) addr_fi, cache_mem_model mem_obj, event clk_e);
+  function new(mailbox #(cache_transaction) addr_fi, cache_mem_model mem_obj, event clk_e);
     this.addr_fifo = addr_fi;
     this.mem = mem_obj;
     this.clk_ev = clk_e;
@@ -15,16 +15,16 @@ class cache_checker;
     forever begin
       @(clk_ev);
       if(cr_inf.ready_core) begin
-        logic[31:0] local_addr;
+        cache_transaction local_tr;
         logic[31:0] expect_data;
         if(!(addr_fifo.num() == 0)) begin
-          addr_fifo.get(local_addr);
-          expect_data = mem.get_data(local_addr);
-          $display("[INF_TR] Testing transaction. Addr: %h | Checking Cache Replacement: %h | Checking Split Addr: %h", local_addr, expect_data.is_replace_check, expect_data.inj_split);
+          addr_fifo.get(local_tr);
+          expect_data = mem.get_data(local_tr.addr);
+          $display("[INF_TR] Testing transaction. Addr: %h | Checking Cache Replacement: %h | Checking Split Addr: %h", local_tr.addr, local_tr.is_replace_check, local_tr.inj_split);
           if(cr_inf.core_rdata == expect_data) begin
             $display("[CHECKER_OK]  Match! Addr: %h | Data: %h", local_addr, cr_inf.core_rdata);
           end else begin
-            $error("[CHECKER_FAIL] MISMATCH! Addr: %h | Exp: %h | Got: %h", local_addr, expect_data, cr_inf.core_rdata);
+            $error("[CHECKER_FAIL] MISMATCH! Addr: %h | Exp: %h | Got: %h", local_tr.addr, expect_data, cr_inf.core_rdata);
           end
         end else begin
           $error("[CHECKER_ERR] Empty FIFO! Cache returned data, but no pending request found.");
