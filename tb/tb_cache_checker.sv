3a77791961e045de98a185eb357427057d784edb
azovsergej8-sketch <azovsergej8@gmail.com>
2026-06-03 17:27:48 +0300

    Update tb_cache_checker.sv

--- tb/tb_cache_checker.sv
+++ tb/tb_cache_checker.sv
@@ -22,7 +22,7 @@ class cache_checker;
           expect_data = mem.get_data(local_tr.addr);
           $display("[INF_TR] Testing transaction. Addr: %h | Checking Cache Replacement: %h | Checking Split Addr: %h", local_tr.addr, local_tr.is_replace_check, local_tr.inj_split);
           if(cr_inf.core_rdata == expect_data) begin
-            $display("[CHECKER_OK]  Match! Addr: %h | Data: %h", local_addr, cr_inf.core_rdata);
+            $display("[CHECKER_OK]  Match! Addr: %h | Data: %h", local_tr.addr, cr_inf.core_rdata);
           end else begin
             $error("[CHECKER_FAIL] MISMATCH! Addr: %h | Exp: %h | Got: %h", local_tr.addr, expect_data, cr_inf.core_rdata);
           end
