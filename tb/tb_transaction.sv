ca2e48dcd1590f9ab1b6e5c27aabbc9530af3d82
azovsergej8-sketch <azovsergej8@gmail.com>
2026-06-03 16:54:50 +0300

    Update tb_transaction.sv

--- tb/tb_transaction.sv
+++ tb/tb_transaction.sv
@@ -2,7 +2,10 @@ class cache_transaction;
   rand logic[31:0] addr;
   rand logic inj_split;
   rand logic is_16_bit;
-
+  rand logic is_replace_check;
+  static logic [31:0] address_history[$]; 
+  static int max_history_size = 16;
+  
   constraint addr_cons{
     if(inj_split) addr[3:0] inside {[4'hE : 4'hF]};
     else addr[3:0] dist {[0:9] :/ 80, [10:15] :/ 20};
@@ -11,4 +14,20 @@ class cache_transaction;
   constraint split_inj_c{
     inject_split_err dist { 1 := 10, 0 := 90 };
   };
+
+  constraint check_replace_c{
+    is_replace_check dist {0 := 30, 0 := 70};
+  };
+
+  function void post_randomize();
+    if(addres_history.size() > 0 && is_replace_check) begin
+      logic[3:0] rand_idex = $urandom_range(0, address_history.size() - 1);
+      addr = {address_history[rand_index][31:4], addr[3:0]};
+    end
+  endfunction
+
+  function void take_addr(logic[31:0] a);
+    if(addres_history.size() == max_size) address_history.pop_front();
+    addres_history.push_back(a);
+  endfunction
 endclass
