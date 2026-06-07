892fac0e6e451efebaa2fdac69cbb1a4f5ddf3b7
azovsergej8-sketch <azovsergej8@gmail.com>
2026-06-07 23:29:18 +0300

    Update tb_cache_driver.sv

--- tb/tb_cache_driver.sv
+++ tb/tb_cache_driver.sv
@@ -7,19 +7,26 @@ class cache_driver;
   
   task drive(virtual core_intf cr_intf, event clk_e);
     cache_transaction tr;
-    repeat(50) begin
+    int transaction_count = 0;
+    while(transaction_count < 50) begin
       if(tr == null) begin
         tr = new();
         if(!tr.randomize()) $error("Randomization failed!");
       end
       @(clk_e);
+      cr_intf.core_valid <= #1 0;
       if(cr_intf.ready_core) begin
-        cr_intf.core_addr <= #1 tr.addr;
-        cr_intf.core_valid <= #1 1;
-        tr_fifo.put(tr);
-        tr.take_addr(tr.addr);
-        tr = null;
-      end else cr_intf.core_valid <= 0;
+      	cr_intf.core_addr <= #1 tr.addr;
+      	cr_intf.core_valid <= #1 1;
+      	tr.take_addr(tr.addr);
+        $display("[INF_TR] {%0d} Testing TR: Addr: %h | Replace Check: %h | Split Check: %h ", transaction_count + 1, tr.addr, tr.is_replace_check, tr.inj_split);
+      	tr_fifo.put(tr);
+        @(clk_e);
+        cr_intf.core_valid <= #1 0;
+        @(clk_e);
+      	tr = null;
+      	transaction_count = transaction_count + 1;
+      end
     end
   endtask
 endclass
