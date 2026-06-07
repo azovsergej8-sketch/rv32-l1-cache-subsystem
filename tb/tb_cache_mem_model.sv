87632f5870b2cb988417cfe7cca3c7afb03ad447
azovsergej8-sketch <azovsergej8@gmail.com>
2026-06-07 23:29:38 +0300

    Update tb_cache_mem_model.sv

--- tb/tb_cache_mem_model.sv
+++ tb/tb_cache_mem_model.sv
@@ -1,23 +1,23 @@
 class cache_mem_model;
-  protected logic[7:0] ram_table[logic[31:0]];
+  protected logic[127:0] ram_table[logic[31:4]];
   
   function logic[31:0] get_data(logic[31:0] addr);
     logic[31:0] result;
-    for(int i = 0; i < 4; i++) begin
-      if(!ram_table.exists(addr + i)) ram_table[addr+ i] = $urandom;
-    end
-    result = {ram_table[addr], ram_table[addr + 1], ram_table[addr + 2], ram_table[addr + 3]};
+    if ($isunknown(addr)) addr = 32'h0;
+    if(!ram_table.exists(addr[31:4])) ram_table[addr[31:4]] = {$urandom, $urandom, $urandom, $urandom};
+    result = ram_table[addr[31:4]][addr[3:2]*32 +: 32];
     return result;
   endfunction
 
   task run_mem_responder(virtual memory_intf mem_if, event e_clk);
     forever begin
       @(e_clk);
-      if(mem_if.mem_valid) begin
-        mem_if.mem_rdata <= this.get_data(mem_if.mem_addr);
-        mem_if.mem_ready <= 1;
+      if(mem_if.mem_valid && !mem_if.mem_ready) begin
+        mem_if.mem_rdata <= #1 this.get_data(mem_if.mem_addr);
+        mem_if.mem_ready <= #1 1;
+      end else begin
+      	mem_if.mem_ready <= #1 0;
       end
-      if(mem_if.mem_ready) mem_if.mem_ready <= 0;
     end
   endtask
 endclass
