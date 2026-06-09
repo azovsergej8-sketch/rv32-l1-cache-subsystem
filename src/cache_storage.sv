4ef23eb197a09d14d37cb1f78d94a0ef1df4238a
azovsergej8-sketch <azovsergej8@gmail.com>
2026-06-09 13:44:34 +0300

    Update cache_storage.sv

--- src/cache_storage.sv
+++ src/cache_storage.sv
@@ -10,6 +10,10 @@ module cache_storage(
   //FSM
   always@(posedge clk or negedge rst) begin
     if(!rst) begin
+      tag[0][0] <= 0; tag[1][0] <= 0; tag[2][0] <= 0; tag[3][0] <= 0;
+      tag[4][0] <= 0; tag[5][0] <= 0; tag[6][0] <= 0; tag[7][0] <= 0;
+      tag[8][0] <= 0; tag[9][0] <= 0; tag[10][0] <= 0; tag[11][0] <= 0;
+      tag[12][0] <= 0; tag[13][0] <= 0; tag[14][0] <= 0; tag[15][0] <= 0;
     end else begin
       if(!inf.storage_we) begin
         inf.storage_rtag <= tag[inf.storage_rindex];
